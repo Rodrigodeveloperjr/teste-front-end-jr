288db256e0652143f260c684164781047b09ffbf
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-03-04 21:50:33 -0300

    fix: fixing font bug

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-	* {
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;800&display=swap');
-    
+	* {    
 		margin: 0;
 		padding: 0;
 		outline: 0;
